f3ed22104715bb764c8dd88ff72e5bceb0b49c9d
rhombus0516 <karaya0913@gmail.com>
2024-02-21 01:30:12 +0000

    [Add]testコントローラー追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "8e01ee58449743bd83818b50df37edda.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
