0e38707374b32038744762ca9b9d1971df2f8391
rhombus0516 <karaya0913@gmail.com>
2024-02-25 02:26:22 +0000

    [Add]swiper導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "script.js"
 import 'jquery'
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
